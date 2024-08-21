f61713e5517be369f1ff1c3b931efa012483b5d6
mayorovyf <26-4b-mayorov@mail.ru>
2024-08-21 11:59:08 +0300

    movement

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    [SerializeField] private float SensX;
+    [SerializeField] private float SensY;
+    [SerializeField] private Transform orientation;
+
+    private float xRotation;
+    private float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X")*SensX*Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y")*SensY*Time.deltaTime;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        
+        transform.rotation = Quaternion.Euler(xRotation,yRotation,0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;   
+    [SerializeField] private float groundDrag;
+    
+    [Header("Ground Check")] 
+    [SerializeField] private float playerHeight;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool gronded;
+    
+    
+    [SerializeField] private Transform orientation;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private Vector3 moveDirection;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        gronded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f+1f,whatIsGround);
+        
+        
+        MyInput();
+        SpeedControl();
+        
+        
+        if (gronded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized*moveSpeed*10f,ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+}
