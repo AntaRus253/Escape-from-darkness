786696247e94f7224796f5d6bca22c0a2d8b9ad9
mayorovyf <26-4b-mayorov@mail.ru>
2024-08-22 20:23:03 +0300

    add sprint

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -10,18 +10,20 @@ public class Controller : MonoBehaviour
 
     [Header("Keybinds")] 
     [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private KeyCode accelerationKey = KeyCode.LeftControl;
     
     [Header("Movement")]
     [SerializeField] private float speed;
+    [SerializeField] private float accelerationBoost;
 
     [Header("Physics")] 
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float gravity;
     [SerializeField] private float startGravity;
     [SerializeField] private float groundDistance=0.3f;
-    
-    
-    
+
+
+    private bool acceleration;
     private bool grounded;
     private CharacterController characterController;
     private Vector3 moveDirection;
@@ -40,24 +42,39 @@ public class Controller : MonoBehaviour
         {
             velocity.y = -startGravity;
         }
-        
+
         MyInput();
         MovePlayer();
     }
 
     private void MovePlayer()
     {
+        
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        
+        if (acceleration)
+        {
+            moveDirection *= accelerationBoost;
+        }
+        
         characterController.Move(moveDirection*speed*Time.deltaTime);
             
-            velocity.y -= gravity * Time.deltaTime;
-            characterController.Move(velocity * Time.deltaTime);
+        velocity.y -= gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
 
     }
     private void MyInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-
+        if (Input.GetKey(accelerationKey))
+        {
+            acceleration = true;
+        }
+        else
+        {
+            acceleration = false;
+        }
+        
     }
 }
